446cb4b4a2d4d4cf6b1d83e3b609a3888eb90a37
Oriol Soler <22oriolsoler@gmail.com>
2022-11-15 10:54:25 +0100

    Restructuracion y lab5 js

---
+++ Apuntes/section-03-js/index.js
@@ -0,0 +1,32 @@
+// console.log("pipo");
+// console.log(document);
+// console.table([
+//     "Bad Bunny" , 7, "J Balvin", false, "Ozuna", undefined,
+// ])
+
+// const me = {name:"Oriol", lastname:"Soler", citiesLivedIn:["Barcelona" , "Esplugues de Llobregat"], age: 23, dob: 1998, drinkAlcohol: true };
+// console.log(me);
+
+
+// const books =   [{bookName:"Jojo's Bizzare Adventure", genre:"Comedy", author:"Araki", year: 1998, ISBN: '312312312JJFSDJS' },
+// {bookName:"Blood and fire", genre:"Fantasy", author:"George R.R Martin", year: 2018, ISBN: 'KHLNSDA312312J' },
+// {bookName:"Todo Arde", genre:"Thriller", author:"Juan Gomez Jurado", year: 2022, ISBN: '78439204HDSAJ' }]
+// console.log(books);
+
+let hobbie;
+const nombre = "Oriol";
+const apellido = "Soler";
+const edad = 23;
+
+
+hobbie = "Me gusta jugar a videojuegos";
+console.log( "Hola mi nombre es " + nombre + " " + apellido + " tengo " + edad + " años y " + hobbie);
+hobbie = "Me gusta la impresion 3d";
+console.log( "Hola mi nombre es " + nombre + " " + apellido + " tengo " + edad + " años y " + hobbie);
+hobbie = "Me gusta la electronica";
+console.log( "Hola mi nombre es " + nombre + " " + apellido + " tengo " + edad + " años y " + hobbie);
+hobbie = "Me gusta hacer kick boxing";
+console.log( "Hola mi nombre es " + nombre + " " + apellido + " tengo " + edad + " años y " + hobbie);
+hobbie = "Me gusta comer";
+console.log( "Hola mi nombre es " + nombre + " " + apellido + " tengo " + edad + " años y " + hobbie);
+
---
+++ Labs/lab-js-basic-algorithms-es/index.js
@@ -0,0 +1,55 @@
+//Iteration 1: Names and Input
+console.log('Iteration 1: Names and Input');
+
+const hacker1 = 'Juaan';
+console.log('The driver name is ' + hacker1);
+const hacker2 = "Pedro";
+console.log('The navigator name is ' + hacker2);
+
+//Iteration 2: Conditional
+console.log("Iteration 2: Conditionals");
+if (hacker1.length > hacker2.length)
+    console.log('The driver ' + hacker1 +' has the longest name, it has ' + hacker1.length);
+else if (hacker2.length > hacker1.length)
+    console.log('The navigator ' + hacker2 +' has the longest name, it has ' + hacker2.length);
+else 
+    console.log('Wow, you both have thequally long names, ' + hacker1.length + ' characters')
+
+//Iteration 3: Loops
+console.log('Iteration 3: Loops');
+//3.1
+console.log('3.1')
+
+let capsName = ""
+
+for (let i = 0; i< hacker1.length; i++) {
+    capsName += hacker1[i].toUpperCase() + ' ';
+}
+console.log(capsName);
+
+//3.2
+console.log('3.2')
+
+let reverseName = ""
+
+for (let i = hacker1.length; i >= 0; i--) {
+    reverseName += hacker1[i];
+}
+
+console.log(reverseName);
+
+//3.3
+console.log('3.3')
+
+function lexicoGraphicOrder (str1, str2) {
+
+    if (str1 > str2) 
+        console.log(" The drivers name goes first ");
+    else if (str1 < str2) 
+        console.log('Yo, the navigator goes first definitely');
+    else
+        console.log('What?! You both have the same name?');
+}
+
+lexicoGraphicOrder (hacker1,hacker2);
+
