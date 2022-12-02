78990426dbe8618b86e5985a7e3bc8f270539571
Oriol Soler <22oriolsoler@gmail.com>
2022-12-02 11:16:25 +0100

    completed

--- Labs/lab-vue-setup/lab-vue-setup/src/components/Ex1.vue
+++ Labs/lab-vue-setup/lab-vue-setup/src/components/Ex1.vue
@@ -1,22 +1,29 @@
+<!-- HTML -->
 <template>
-    <button @click="increaseCount">count is: {{ state.count }}</button>
-  </template>
-   
-  <script lang="ts">
-  import { ref, defineComponent } from "vue";
-   
-  export default defineComponent({
-    name: "App",
-    setup: () => {
-      const state = ref({
-        count: 0
-      });
-   
-      const increaseCount = () => {
-        state.value.count++;
-      }
-   
-      return { state, increaseCount };
-    },
-  });
-  </script>
+    <h1>{{ msg }}</h1>
+    <img style='width: 100px' v-bind:src="meme.image" :alt="meme.alt" />
+
+    <!-- <button v-on:click="wuTang()">Click me - LONG SYNTAX</button> -->
+    <!-- Short Version - SYNTAX -->
+    <button @click="buttonAlert">Alerta - pulsame!</button>
+</template>
+<!-- JS -->
+<script setup>
+import { ref, reactive } from "vue";
+//EX2
+const msg = ref("Hola me llamo Oriol");
+const meme = reactive({
+  image: "https://ih1.redbubble.net/image.2955202663.5204/fposter,small,wall_texture,product,750x1000.jpg",
+  alt: "default-image",
+});
+
+//EX3
+const buttonAlert = () => {
+  alert("Sal de mi cienaga");
+};
+</script>
+<!-- CSS -->
+<style>
+
+
+</style>
